d96712f07ad794bb383a3ea34cc70c68df304416
Leonardo Vidarte <lvidarte@gmail.com>
2012-07-29 20:47:18 -0300

    Changed path to local options file

--- laiserver/__init__.py
+++ laiserver/__init__.py
@@ -23,6 +23,7 @@ from tornado.options import define
 version = '0.1.0'
 
 define("base_dir", os.path.realpath(os.path.dirname(__file__)))
+define("home_dir", os.path.expanduser('~'))
 
 define("debug", True)
 define("addr", '127.0.0.1')
@@ -33,10 +34,10 @@ define("db_port", 27017)
 define("db_name", "lai_dev")
 define("db_collection", "server")
 
-define("prv_key", os.path.join(os.path.expanduser('~'), ".ssh/id_rsa"))
+define("prv_key", os.path.join(options.home_dir, ".ssh/id_rsa"))
 
 try:
-    options_file = os.path.join(options.base_dir, '/etc/default/lai-server')
+    options_file = os.path.join(options.home_dir, '.lai-server')
     tornado.options.parse_config_file(options_file)
 except:
     pass
